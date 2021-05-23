384e1127f73f128a08d407e7470e211da0bb6375
ihyavorskyi <ihor201324@gmail.com>
2021-05-23 16:02:59 +0300

    Some fixes

--- GroupProject/scripts/load-common-elements.js
+++ GroupProject/scripts/load-common-elements.js
@@ -4,4 +4,10 @@
         function (response) {
             document.getElementById("header").innerHTML = response
         });
+
+    $ajaxUtils.sendGetRequest(
+        "snippets/footer-snippet.html",
+        function (response) {
+            document.getElementById("footer").innerHTML = response
+        });
 }())
